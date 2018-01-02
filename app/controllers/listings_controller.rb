b2819857777225b5298f9ba0373cf6af37349ff6
github username <github email address>
2018-01-02 15:45:20 -0500

    listing controler methods

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,2 +1,29 @@
 class ListingsController < ApplicationController
+  def index
+
+    @listing = Listing.all
+    render json: @listing
+  end
+
+  def create
+    @listing = Listing.new(listing_params)
+    # user = User.all.find{|i| i.id === @listing.userId}
+    # @listing.user_id = user
+    if @listing.save
+
+      render json: @listing
+    else
+      render json: {errors: @listing.errors.full_messages}
+    end
+  end
+
+  def destroy
+    @listing = Listing.find(params[:id])
+    @listing.destroy
+  end
+
+  private
+  def listing_params
+    params.require(:listing).permit(:title, :picture, :price, :availability, :location)
+  end
 end
