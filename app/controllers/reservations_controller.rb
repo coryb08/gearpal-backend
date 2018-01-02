77224aadede36006e0a356c32123389250419995
github username <github email address>
2018-01-02 16:00:02 -0500

    reservation controller methods

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,2 +1,34 @@
 class ReservationsController < ApplicationController
+  def index
+
+    @reservation = Reservation.all
+    render json: @reservation
+  end
+
+  def create
+    @reservation = Reservation.new(reservation_params)
+    # user = User.all.find{|i| i.id === @reservation.userId}
+    # @reservation.user_id = user
+    if @reservation.save
+
+      render json: @reservation
+    else
+      render json: {errors: @reservation.errors.full_messages}
+    end
+  end
+
+  def update
+    @reservation = Reservation.find(params[:id])
+    @reservation.rating = params[:rating]
+  end
+
+  def destroy
+    @reservation = Reservation.find(params[:id])
+    @reservation.destroy
+  end
+
+  private
+  def reservation_params
+    params.require(:reservation).permit(:start_date, :end_date)
+  end
 end
