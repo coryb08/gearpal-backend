f70d9875d5ee19d71ed6befea4defcfd9b3be9d7
erickalexander <erickcamacho1@gmail.com>
2018-01-02 15:52:21 -0500

    updated associations in models

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,3 +1,5 @@
 class Listing < ApplicationRecord
   belongs_to :user
+  has_many :listing_reservations
+  has_many :reservations, through: :listing_reservations
 end
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,3 +1,5 @@
 class Reservation < ApplicationRecord
   belongs_to :user
+  has_many :listing_reservations
+  has_many :listings, through: :listing_reservations
 end
