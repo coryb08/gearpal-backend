989946264b30a3848ddd3f7fb8f84d3e36cded95
erickalexander <erickcamacho1@gmail.com>
2018-01-03 16:12:37 -0500

    fixed serializers

--- app/serializers/listing_serializer.rb
+++ app/serializers/listing_serializer.rb
@@ -1,5 +1,5 @@
 class ListingSerializer < ActiveModel::Serializer
-  attributes :id, :title, :picture, :price, :availability, :location, :condition, :name
+  attributes :id, :title, :picture, :price, :availability, :location, :condition, :rating
   belongs_to :lister, polymorphic: true
   has_many :reservations
 end
