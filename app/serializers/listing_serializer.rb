d702a554f5ccbf395e02d8103c86a25fe34a5fd4
github username <github email address>
2018-01-03 16:57:29 -0500

    added lister_id to listing json

--- app/serializers/listing_serializer.rb
+++ app/serializers/listing_serializer.rb
@@ -1,5 +1,5 @@
 class ListingSerializer < ActiveModel::Serializer
-  attributes :id, :title, :picture, :price, :availability, :location, :condition, :rating
+  attributes :id, :title, :picture, :price, :availability, :location, :condition, :rating, :lister_id
   belongs_to :lister, polymorphic: true
   has_many :reservations
 end
