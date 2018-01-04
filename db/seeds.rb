825cd411fda8c92961f2c34e9d9f3dfb98b5f686
dansbands <dansbands@hotmail.com>
2018-01-03 20:04:04 -0500

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,6 +23,11 @@ listing5 = Listing.create(lister: u1, title: "Shure SM 58", picture: "https://52
 listing6 = Listing.create(lister: u1, title: "Shure SM 87A", picture: "https://522bb370f5443d4fe5b9-f62de27af599bb6703e11b472beadbcc.ssl.cf2.rackcdn.com/product/list_image/7686/prod_img_sm87a_m.jpg", price: 20, availability: true, location: "Brooklyn, NY", condition: 'Worn',rating: 3)
 listing7 = Listing.create(lister: u1, title: "Shure SM 58", picture: "https://522bb370f5443d4fe5b9-f62de27af599bb6703e11b472beadbcc.ssl.cf2.rackcdn.com/product/list_image/10785/prod_img_sm58_m.jpg", price: 10, availability: true, location: "Staten Island, NY", condition: 'Fair',rating: 3)
 listing8 = Listing.create(lister: u1, title: "Shure SM 87A", picture: "https://522bb370f5443d4fe5b9-f62de27af599bb6703e11b472beadbcc.ssl.cf2.rackcdn.com/product/list_image/7686/prod_img_sm87a_m.jpg", price: 20, availability: true, location: "Brooklyn, NY", condition: 'Worn',rating: 1)
+listing9 = Listing.create(lister: u1, title: "Shure SM 58", picture: "https://522bb370f5443d4fe5b9-f62de27af599bb6703e11b472beadbcc.ssl.cf2.rackcdn.com/product/list_image/10785/prod_img_sm58_m.jpg", price: 10, availability: true, location: "Brooklyn, NY", condition: 'Fair',rating: 3)
+listing10 = Listing.create(lister: u1, title: "Shure SM 87A", picture: "https://522bb370f5443d4fe5b9-f62de27af599bb6703e11b472beadbcc.ssl.cf2.rackcdn.com/product/list_image/7686/prod_img_sm87a_m.jpg", price: 20, availability: true, location: "Staten Island, NY", condition: 'Worn',rating: 3)
+listing11 = Listing.create(lister: u1, title: "Shure SM 58", picture: "https://522bb370f5443d4fe5b9-f62de27af599bb6703e11b472beadbcc.ssl.cf2.rackcdn.com/product/list_image/10785/prod_img_sm58_m.jpg", price: 10, availability: true, location: "Brooklyn, NY", condition: 'Fair',rating: 3)
+listing12 = Listing.create(lister: u1, title: "Shure SM 87A", picture: "https://522bb370f5443d4fe5b9-f62de27af599bb6703e11b472beadbcc.ssl.cf2.rackcdn.com/product/list_image/7686/prod_img_sm87a_m.jpg", price: 20, availability: true, location: "Staten Island, NY", condition: 'Worn',rating: 1)
+
 
 Reservation.destroy_all
 
