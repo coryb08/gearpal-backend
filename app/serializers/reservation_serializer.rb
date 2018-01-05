c166dc980a96bd94a9e9ed2fc4b9af2a42b8fca2
github username <github email address>
2018-01-05 14:28:08 -0500

    working backend

--- app/serializers/reservation_serializer.rb
+++ app/serializers/reservation_serializer.rb
@@ -1,5 +1,5 @@
 class ReservationSerializer < ActiveModel::Serializer
-  attributes :id, :start_date, :end_date, :listing_id
+  attributes :id, :start_date, :end_date, :listing_id, :name
   belongs_to :reserver, polymorphic: true
   has_many :listings
 end
